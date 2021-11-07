8feb676cb3c360dc94b3873db7286fd4d20e9ec7
hernan87ok <hernancordary@gmail.com>
2021-11-07 17:07:45 -0300

    Se aniade /products

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,27 +1,3 @@
-
-// let platos = [
-//   {
-//   nombre: 'Carpaccio fresco',
-//   descripcion: 'Entrada Carpaccio de salmón con cítricos',
-//   precio: 'U$S 65.50',
-//   },
-//   {
-//   nombre: 'Risotto de berenjena',
-//   descripcion: 'Risotto de berenjena y queso de cabra ',
-//   precio: 'U$S 47.00',
-//   },
-//   {
-//   nombre: 'Mousse de arroz',
-//   descripcion: 'Mousse de arroz con leche y aroma de azahar ',
-//   precio: 'U$S 27.50',
-//   },
-//   {
-//   nombre: 'Espárragos blancos',
-//   descripcion: 'Espárragos blancos con vinagreta de verduras y jamón ibérico',
-//   precio: 'U$S 37.50',
-//   }];
-
-
 const fs = require('fs');
 const path = require('path');
 
@@ -43,13 +19,9 @@ const mainController = {
 
     home: (req, res) => {
       res.render('./main/home.ejs');
-    },
-
-    index: (req, res) => {
-      res.render('./main/index.ejs', {
-        productsSent: products
-      });
     }
+
+
 };
 
 module.exports = mainController;
